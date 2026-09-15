f266d08778f75c44b8dd8c4dc87ffe1e00e68114
chEnru0524 <r10106666@gmail.com>
2026-09-15 15:09:21 +0800

    Add React application entry point

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { StoreProvider } from "./store";
+import App from "./App";
+import "./style.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <StoreProvider>
+        <App />
+      </StoreProvider>
+    </HashRouter>
+  </React.StrictMode>,
+);
